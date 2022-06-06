9c0470be374878a80ca8157fa5a6f3f8aa85bedb
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-06-06 09:48:34 -0400

    edit name file icons

--- src/views/signIn.js
+++ src/views/signIn.js
@@ -3,14 +3,16 @@ import { replaceHTML } from '../utils/utils.js';
 import { addEvents } from '../events/sigInEvents.js';
 
 const getHTMLTemplate = () => {
-  const template = `<div id="containerSignIn">
+  const template = /*html*/
+    `<div id="containerSignIn">
+  <img id='back' src='icons/volver.png' alt="MDN">
   <h3>Email:</h3>
-  <input id= 'email' type="email" placeholder="ingrsa tu email">
+  <input id= 'email' type="email" placeholder="Ingresa tu email">
   <h3>Contraseña:</h3>
   <input id='password' type="password" placeholder="contraseña">
 </div>
 <button id='send'>Iniciar</button>
-<img id='logoSingIn' src='imagenes/kitty.png'> </img>`;
+<img id='logoSingIn' src='icons/kitty.png'> </img>`;
   return template;
 };
 const SignIn = () => {
