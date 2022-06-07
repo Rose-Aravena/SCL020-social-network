c0d8fafbc1ed340095edb4f2beffcffd9e97f8e0
Fernanda <fernanda@MacBook-Air-de-Fernanda.local>
2022-06-07 09:58:32 -0400

    adding templates

--- src/views/creatUser.js
+++ src/views/creatUser.js
@@ -3,14 +3,31 @@
 import { addEvents } from '../events/creatUserEvents.js';
 import { replaceHTML } from '../utils/utils.js';
 
-const getHTMLTemplate = () => `<div id="containerCreatUser">
-  <input id='userName' type="text" placeholder="Nombre de usuario">
-  <input id='email' type="email" placeholder="ingrsa tu email">
-  <input id='password' type="password" placeholder="contraseña">
-</div>
-<button id='creatSend'>Registrar</button>
-<img id='logoreatUser' src='icons/veterinary.png'> </img>`;
-
+const getHTMLTemplate = () => {
+  const template = //html
+    `<div id='containerCreatUser'>
+    <div class='btnBackDog'>
+  <img class='backIcon' id='back' src='icons/volver.png'>
+  <p><strong>Volver</strong></p>
+  </div>
+  <form class='containerInputs' id='formCreateUser'>
+      <p>Nombre</p>
+      <input class='inputCreatUser' id='userName' type="text" placeholder="papitas">
+      <p>E-mail</p>
+      <input class='inputCreatUser' id='email' type="email" placeholder="papitas@gmail.com">
+      <p>Constraseña</p>
+     <input class='inputCreatUser' id='password' type="password" placeholder="******">
+     </form>
+     <section id='sectionCreatUser'>
+     <button class='thinButton' id='creatSend'>Registrar</button>
+     <div id='logoCreatUser'>
+     <img  id='iconCreatUser' src='icons/veterinary.png'> </img>
+     </div>
+     </section>
+    
+    </div>`;
+  return template;
+};
 const CreatUser = () => {
   const template = getHTMLTemplate();
   replaceHTML(template);
--- src/views/signIn.js
+++ src/views/signIn.js
@@ -5,17 +5,20 @@ import { addEvents } from '../events/sigInEvents.js';
 const getHTMLTemplate = () => {
   const template = // html
     `<div id='containerSignIn'>
-  <img class='back' id='back' src='icons/volver.png' alt="MDN">
+    <div id='btnBackDog'>
+  <img class='backIcon' id='back' src='icons/volver.png' alt="MDN">
+  <p><strong>Volver</strong></p>
+  </div>
   <div class='containerInputs'>
-  <h1 class='title'>Inicia Sesión </h1>
+  <h1 class='title'>Inicia Sesión</h1>
   <h3 class='subTitle'>Email:</h3>
   <input id= 'email' placeholder="xxx@gmail.com" type="email" >
   <h3 class='subTitle'>Contraseña:</h3>
   <input id='password'  placeholder="******" type="password">
 </div>
-<section>
+<section id='sectionSignIn'>
 <button class='thinButton' id='send'>Iniciar</button>
-<div id='logoSingIn'>
+<div class='logoSingIn'>
 <img src='icons/kitty.png'> </img>
 <div/>
 </section>
